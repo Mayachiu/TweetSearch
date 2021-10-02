b667408b20ca916b18748d58bd1d48220246c343
Mayachiu <mayachiu5g@gmail.com>
2021-10-02 19:43:43 +0900

    Initial Commit

---
+++ TweetSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TweetSearch
+//
+//  Created by 内山和輝 on 2021/10/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
